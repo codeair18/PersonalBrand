914b7a8d9d9704c87af11f25593845f672f09716
codeair18 <codeair18@gmail.com>
2024-04-02 18:04:58 +0200

    Initialization commit, add modules and skeleton

---
+++ app.config.ts
@@ -0,0 +1,7 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'lime',
+    gray: 'neutral',
+  },
+  modules: ['@nuxtjs/i18n']
+})
---
+++ i18n.config.ts
@@ -0,0 +1,12 @@
+export default defineI18nConfig(() => ({
+    legacy: false,
+    locale: 'en',
+    messages: {
+        en: {
+            aboutMe: 'I build pixel-perfect, engaging, and accessible digital experiences.'
+        },
+        pl: {
+            aboutMe: 'Buduję wydajne, ispirujące rozwiązania dla firm.'
+        }
+    }
+}))
