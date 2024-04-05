7f7c10be715997173c0e6f09ebe0ee580f8170bc
codeair18 <codeair18@gmail.com>
2024-04-05 22:47:36 +0200

    comment trash, translations

--- i18n.config.ts
+++ i18n.config.ts
@@ -3,10 +3,12 @@ export default defineI18nConfig(() => ({
     locale: 'pl',
     messages: {
         en: {
-            aboutMe: 'As a team-oriented Full Stack developer, I have a wide range of skills covering both front-end and back-end of applications. I actively participate in the software development process, collaborating with other team members. My versatility allows me to adapt to various project requirements, enabling effective communication and achieving common goals.'
+            aboutMe: 'As a team-oriented Full Stack developer, I have a wide range of skills covering both front-end and back-end of applications. I actively participate in the software development process, collaborating with other team members. My versatility allows me to adapt to various project requirements, enabling effective communication and achieving common goals.',
+            aboutMeExtended: 'Since childhood fascinated by computers and rapidly evolving technology. In 2016, I decided to try my hand at software development, plunging into the vast realm of coding. Evolving swiftly to today, I\'ve had the pleasure of building software for large corporations, startups, and software houses.',
         },
         pl: {
-            aboutMe: 'Jako Full Stack developer działający zespołowo, mam szeroki zakres umiejętności obejmujących front-end i back-end aplikacji. Aktywnie uczestniczę w procesie tworzenia oprogramowania, współpracując z innymi członkami zespołu. Moja wszechstronność pozwala mi dostosowywać się do różnych wymagań projektowych, co umożliwia efektywną komunikację i osiąganie wspólnych celów.'
+            aboutMe: 'Jako Full Stack developer działający zespołowo, mam szeroki zakres umiejętności obejmujących front-end i back-end aplikacji. Aktywnie uczestniczę w procesie tworzenia oprogramowania, współpracując z innymi członkami zespołu. Moja wszechstronność pozwala mi dostosowywać się do różnych wymagań projektowych, co umożliwia efektywną komunikację i osiąganie wspólnych celów.',
+            aboutMeExtended: 'Od dziecka zafascynowany komputerami oraz dynamicznie rozwijającą się technologią. W 2016 roku postanowiłem spróbować swoich sił w tworzeniu oprogramowania, zapadłem się wtedy rozległą krainę kodowania. Rozwijając się szybko do dzisiaj, miałem przyjemność budowania oprogramowania dla dużych korporacji, start-up\'ów oraz softwarehouse\'ów',
         }
     }
 }))
