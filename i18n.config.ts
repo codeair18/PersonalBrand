08978284d3683e9424508e96b2179537a3d5212a
codeair18 <50672237+codeair18@users.noreply.github.com>
2024-11-04 20:57:10 +0100

    change favicon.svg (#1)
    * change favicon.svg
    
    * add more copy

--- i18n.config.ts
+++ i18n.config.ts
@@ -4,8 +4,8 @@ export default defineI18nConfig(() => ({
     messages: {
         pl: {
             aboutMe: 'Pomagam w tworzeniu rozwiązań skrojonych na miarę biznesu oraz optymalizuje procesy ich wdrażania.',
-            aboutMeExtended: 'Od dziecka zafascynowany komputerami oraz dynamicznie rozwijającą się technologią. W 2016 roku postanowiłem spróbować swoich sił w tworzeniu oprogramowania, zapadłem się wtedy rozległą krainę kodowania. Rozwijając się szybko do dzisiaj, miałem przyjemność budowania oprogramowania dla dużych korporacji, start-up\'ów oraz softwarehouse\'ów',
-            techDescription: 'Uwielbiam prace zespołową, wymianę spostrzerzeń oraz rozwiązywanie skomplikowanych problemów. W procesie wytwarzania kodu wykorzystuję DDD , CQRS, SOLID oraz architekturę heksagonalną. Zintegrowanie tych technik pozwala na tworzenie bardziej wydajnych, skalowalnych i stabilnych aplikacji.',
+            aboutMeExtended: 'Od dziecka zafascynowany komputerami oraz dynamicznie rozwijającą się technologią. W 2016 roku po uzyskaniu dyplomu inżyniera w zakresie geodezji, postanowiłem zmienić branżę. Wwiercając się w szeroko pojęty świat IT, miałem przyjemność budowania oprogramowania dla dużych korporacji, start-up\'ów oraz softwarehouse\'ów',
+            techDescription: 'Uwielbiam prace zespołową, wymianę spostrzerzeń oraz rozwiązywanie skomplikowanych problemów. W procesie wytwarzania kodu wykorzystuję najlepsze praktyki w celu zapewniania stabilność, skalowalności oraz łatwego dostępu do funkcjonalności aplikacji',
             freeTime: 'Gdy nie jestem przy komputerze, zazwyczaj spędzam czas aktywanie poza domem, gotuję włoskie dania lub spaceruję z <a href="https://www.instagram.com/pies_hermes" target="_blank">Hermesem.</a>',
             konsultExpert: 'Rozwijanie platformy medycznej umożliwiającej konsultację on-line. Tworzenie nowych funkcjonalności zarówno po stronie backendowej jak i frontendowej.',
             telepolis: 'Przeprojektowanie oraz przepisanie telekomunikacyjnego serwisu informacyjnego na nową technologię.',
@@ -16,12 +16,13 @@ export default defineI18nConfig(() => ({
             silkyCoders: 'Przeprojektowanie starych, mało wydajnych funkcjonalności oraz rozwój wewnętrznej aplikacji do zarządzania produktami detalicznymi.',
             umbrellAi: 'Rozwój aplikacji agregującej towarzystwa ubzpieczeniowe, która umożliwia zakup polisy przez internet',
             firstComperia: 'Przeprojektowanie oraz przepisanie telekomunikacyjnego serwisu informacyjnego na nową technologię. Rozwijanie aplikacji webowej umożliwiającej otrzymanie pożyczki gotówkowej w procesie on-line. Współprojketowanie architektury, rozwój instytucji płatnicznej agregującej płatności odroczone.',
-            secondComperia: 'TODO',
+            comperiaPay: 'Polska platforma finansowa oferująca rozwiązania płatnicze i finansowanie zakupów dla e-commerce i usług. Umożliwia klientom płatności ratalne, co wspiera rozwój sprzedaży online i ułatwia dokonywanie większych zakupów. Platforma zapewnia łatwą integrację z systemami sklepów oraz przejrzyste i bezpieczne warunki transakcji.',
+            cvior: 'Cvior umożliwia generowanie CV na podstawie profilu LinkedIn lub pliku PDF, automatycznie przetwarzając dane i uzupełniając szablony. Użytkownik może dostosować wygląd i treść CV, wybierając różne style i układy'
         },
         en: {
             aboutMe: 'I help create tailor-made solutions for the business and optimise the implementation processes.',
-            aboutMeExtended: 'Since childhood fascinated by computers and rapidly evolving technology. In 2016, I decided to try my hand at software development, plunging into the vast realm of coding. Evolving swiftly to today, I\'ve had the pleasure of building software for large corporations, startups, and software houses.',
-            techDescription: 'I love teamwork, exchanging insights, and solving complex problems. In the process of code development, I utilize DDD, CQRS, SOLID, and hexagonal architecture. Integrating these techniques allows for the creation of more efficient, scalable, and stable applications.',
+            aboutMeExtended: 'Fascinated by computers and rapidly developing technology since childhood. In 2016, after earning my engineering degree in geodesy, I decided to change my field. Drilling into the broad world of IT, I had the pleasure of developing software for large corporations, startups, and software houses.',
+            techDescription: 'I love teamwork, exchanging insights, and solving complex problems. In the coding process, I use best practices to ensure the stability, scalability, and easy access to the application’s functionalities.',
             freeTime: 'When I\'m not at the computer, I usually spend time actively outdoors, cooking Italian dishes, or taking walks with <a href="https://www.instagram.com/pies_hermes" target="_blank">Hermes.</a>',
             konsultExpert: 'Developing a medical platform that enables online consultation. Creating new functionalities on both backend and frontend side.',
             telepolis: 'Redesign and rewrite of telecommunications information service to new technology.',
@@ -32,7 +33,8 @@ export default defineI18nConfig(() => ({
             silkyCoders: 'Redesign of old, inefficient functionality and development of internal product management application.',
             umbrellAi: 'Development of an application aggregating insurance companies, which allows the purchase of a policy online.',
             firstComperia: 'Redesign and rewrite of telecommunications information service to new technology. Developing a web application to obtain a cash loan through an online process. Co-projection of architecture, development of payment institution aggregating deferred payments.',
-            secondComperia: 'TODO',
+            comperiaPay: 'Polish financial platform offering payment solutions and purchase financing for e-commerce and services. It enables customers to make installment payments, supporting online sales growth and facilitating larger purchases. The platform ensures easy integration with store systems and provides transparent and secure transaction conditions.',
+            cvior: 'Cvior allows users to generate a CV based on their LinkedIn profile or a PDF file, automatically processing data and filling in templates. The user can customize the appearance and content of the CV by choosing different styles and layouts.'
         },
     }
 }))
